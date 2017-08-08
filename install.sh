f2bfa80b9bc42457a5291c64d4a4519f4e9a7a96
Marti Markov <marti.markov@icloud.com>
2017-08-08 19:06:23 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z ${ff_bashrc_version+x} ]; then 
+  echo "FF Bash is not setup";
+  echo "source $HOME/.ff_bash/.bashrc" >> $HOME/.bashrc
+else 
+  echo "FF Bash version is '$ff_bashrc_version'";
+fi
+
+source $HOME/.bashrc
